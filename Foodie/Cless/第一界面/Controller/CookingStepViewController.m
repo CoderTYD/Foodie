b9b97b23ca4cd0b5275e6c0d5d9744f232e1bea7
hezhongcheng <576999271@qq.com>
2016-07-21 17:43:35 +0800

    三级界面

--- Foodie/Cless/第一界面/Controller/CookingStepViewController.m
+++ Foodie/Cless/第一界面/Controller/CookingStepViewController.m
@@ -7,17 +7,53 @@
 //
 
 #import "CookingStepViewController.h"
+#import "CookingStepTableViewCell.h"
+@interface CookingStepViewController ()<UITableViewDataSource,UITableViewDelegate,UICollectionViewDataSource,UICollectionViewDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *albumsImage;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tagsLabel;
+@property (strong, nonatomic) IBOutlet UITableView *cookingStepTV;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *height;
+@property (strong, nonatomic) IBOutlet UIView *rootView;
 
-@interface CookingStepViewController ()
 
 @end
 
 @implementation CookingStepViewController
 
+//-(void)updateViewConstraints{
+//    [super updateViewConstraints];
+//    self.height.constant = arc4random() %1000;
+//}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+
+    
+    [self.cookingStepTV registerNib:[UINib nibWithNibName:@"CookingStepTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"cell"];
+    
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 7;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 200;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CookingStepTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
